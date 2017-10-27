7ce5679dc84c6cefd25b2c922efce2cdc99fdc1e
Shreyansh Khajanchi <shreyansh_k@live.com>
2017-10-27 22:47:02 +0530

    add homepage

--- fx_panel.py
+++ fx_panel.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -8,5 +8,18 @@ def hello_world():
 	return 'Hello World!'
 
 
+@app.route("/control/arms/register", methods=["GET", "POST"])
+def register_arm():
+	if request.method == 'POST':
+		pass
+	else:
+		return render_template('register_arm.j2')
+
+
+@app.route("/", methods=["GET"])
+def render_home():
+	return render_template('index.html')
+
+
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
