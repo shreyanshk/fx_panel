2b63a2851cfe5e61cc9d1f1c455bc0239420fc4d
Shreyansh Khajanchi <shreyansh_k@live.com>
2017-10-28 23:18:33 +0530

    update endpoints

--- fx_panel.py
+++ fx_panel.py
@@ -5,17 +5,23 @@ from api import view as apiview
 app = Flask(__name__)
 app.register_blueprint(apiview)
 
+
 @app.route("/", methods=["GET"])
 def render_home():
 	return render_template('homepage.j2')
 
 
-@app.route("/control/arms/register", methods=["GET", "POST"])
+@app.route("/control/arms/addtxn", methods=["GET", "POST"])
 def register_arm():
 	if request.method == 'POST':
 		return "done"
 	else:
 		return render_template('register_arm.j2')
+
+
+@app.route("/control/arms/findarm", methods=["GET"])
+def find_arm():
+	return render_template("find-arm.j2")
 
 
 @app.after_request
