d86133aac15a4d1087310276efa8c5059bb2b059
Shreyansh Khajanchi <shreyansh_k@live.com>
2017-10-28 14:24:04 +0530

    update project

--- fx_panel.py
+++ fx_panel.py
@@ -3,9 +3,9 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-	return 'Hello World!'
+@app.route("/", methods=["GET"])
+def render_home():
+	return render_template('homepage.j2')
 
 
 @app.route("/control/arms/register", methods=["GET", "POST"])
@@ -14,11 +14,6 @@ def register_arm():
 		pass
 	else:
 		return render_template('register_arm.j2')
-
-
-@app.route("/", methods=["GET"])
-def render_home():
-	return render_template('index.html')
 
 
 if __name__ == '__main__':
